d6a55be46793d9a02a73578292149d3ff750d0b0
Mahmoud Abdelhameed <edgepro.mhameed@gmail.com>
2024-09-16 02:27:33 +0300

    Getting ready for tailwindcss

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,12 @@
-function Employee(){
-    return <h3>Here is an Employee !</h3>;
+function Employee(props){
+    return (
+        <>
+            <h3>Employee {props.name}</h3>
+            <p>{props.role ? props.role : "No Role"}</p>
+            {/* {props.role ? <p className="role">{props.role}</p> : <p className="norole">No Role</p>} */}
+            
+        </>
+    )    
 }
 
 export default Employee;
