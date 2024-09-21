43b82726a5d0c37e938ba1d1bd57993730cf581b
Mahmoud Abdelhameed <edgepro.mhameed@gmail.com>
2024-09-22 01:57:24 +0300

    finish edit model

--- src/App.js
+++ src/App.js
@@ -7,36 +7,53 @@ function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Caleb",
       role: "Developer",
       img: "http://localhost:400/image1.jpg",
     },
     {
+      id: 2,
       name: "Sal",
       role: "Manager",
       img: "http://localhost:400/image2.jpg",
     },
     {
+      id: 3,
       name: "john",
       role: "Director of Eng.",
       img: "http://localhost:400/image3.jpg",
     },
     {
+      id: 4,
       name: "James",
       role: "Software Engineer",
       img: "http://localhost:400/image4.jpg",
     },
     {
+      id: 5,
       name: "Corey",
       role: "The DevOps Guy",
       img: "http://localhost:400/image5.jpg",
     },
     {
+      id: 6,
       name: "Jake",
       role: "Senior Intern",
       img: "http://localhost:400/image6.jpg",
     },
   ]);
+
+function updateEmployee(id, newName, NewRole){
+  const updatedEmployees = employees.map((employee) => {
+    if(id == employee.id){
+      return {...employee, name: newName, role: NewRole};
+    }
+    return employee;
+  });
+  setEmployees(updatedEmployees);
+}
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -45,19 +62,19 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              console.log(e.target.value);
               setRole(e.target.value);
             }}
           />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
-              console.log(uuidv4());
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
