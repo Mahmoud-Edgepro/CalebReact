44c63e4e244d261df99b3510e1e9f38b2dc5e0eb
Mahmoud Abdelhameed <edgepro.mhameed@gmail.com>
2024-10-27 01:28:40 +0300

    introduce pages and children props

--- src/App.js
+++ src/App.js
@@ -4,106 +4,14 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
+import Header from "./components/Header";
+import Employees from "./pages/Employees";
 
 function App() {
-  const [role, setRole] = useState("dev");
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: "Caleb",
-      role: "Developer",
-      img: "http://localhost:400/image1.jpg",
-    },
-    {
-      id: 2,
-      name: "Sal",
-      role: "Manager",
-      img: "http://localhost:400/image2.jpg",
-    },
-    {
-      id: 3,
-      name: "john",
-      role: "Director of Eng.",
-      img: "http://localhost:400/image3.jpg",
-    },
-    {
-      id: 4,
-      name: "James",
-      role: "Software Engineer",
-      img: "http://localhost:400/image4.jpg",
-    },
-    {
-      id: 5,
-      name: "Corey",
-      role: "The DevOps Guy",
-      img: "http://localhost:400/image5.jpg",
-    },
-    {
-      id: 6,
-      name: "Jake",
-      role: "Senior Intern",
-      img: "http://localhost:400/image6.jpg",
-    },
-  ]);
-
-  function updateEmployee(id, newName, NewRole) {
-    const updatedEmployees = employees.map((employee) => {
-      if (id == employee.id) {
-        return { ...employee, name: newName, role: NewRole };
-      }
-      return employee;
-    });
-    setEmployees(updatedEmployees);
-  }
-
-  function newEmployee(name, role, img) {
-    const newEmployee = {
-      id: uuidv4(),
-      name: name,
-      role: role,
-      img: img,
-    };
-    setEmployees([...employees, newEmployee]);
-  }
-  const showEmployees = true;
   return (
-    <div className="App">
-      {showEmployees ? (
-        <>
-          <input
-            type="text"
-            onChange={(e) => {
-              setRole(e.target.value);
-            }}
-          />
-          <div className="flex flex-wrap justify-center">
-            {employees.map((employee) => {
-              const editEmployee = (
-                <EditEmployee
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  updateEmployee={updateEmployee}
-                />
-              );
-              return (
-                <Employee
-                  key={employee.id}
-                  id={employee.id}
-                  name={employee.name}
-                  role={employee.role}
-                  img={employee.img}
-                  editEmployee={editEmployee}
-                />
-              );
-            })}
-          </div>
-          <AddEmployee newEmployee={newEmployee} />
-        </>
-      ) : (
-        <p>You cannot see the employees</p>
-      )}
-    </div>
+    <Header>
+      <Employees />
+    </Header>
   );
 }
 
---
+++ src/pages/Employees.js
@@ -0,0 +1,105 @@
+import "../index.css";
+import Employee from "../components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "../components/AddEmployee";
+import EditEmployee from "../components/EditEmployee";
+import Header from "../components/Header";
+
+function Employees() {
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Caleb",
+      role: "Developer",
+      img: "http://localhost:400/image1.jpg",
+    },
+    {
+      id: 2,
+      name: "Sal",
+      role: "Manager",
+      img: "http://localhost:400/image2.jpg",
+    },
+    {
+      id: 3,
+      name: "john",
+      role: "Director of Eng.",
+      img: "http://localhost:400/image3.jpg",
+    },
+    {
+      id: 4,
+      name: "James",
+      role: "Software Engineer",
+      img: "http://localhost:400/image4.jpg",
+    },
+    {
+      id: 5,
+      name: "Corey",
+      role: "The DevOps Guy",
+      img: "http://localhost:400/image5.jpg",
+    },
+    {
+      id: 6,
+      name: "Jake",
+      role: "Senior Intern",
+      img: "http://localhost:400/image6.jpg",
+    },
+  ]);
+
+  function updateEmployee(id, newName, NewRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: NewRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+  const showEmployees = true;
+  return (
+    <div className="App bg-gray-300 min-h-screen">
+      {showEmployees ? (
+        <>
+          <div className="flex flex-wrap justify-center my-2">
+            {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  editEmployee={editEmployee}
+                />
+              );
+            })}
+          </div>
+          <AddEmployee newEmployee={newEmployee} />
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
+    </div>
+  );
+}
+
+export default Employees;
