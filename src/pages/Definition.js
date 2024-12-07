4f4e8610cf3a732fd2319e431d83d3afde3a269c
Mahmoud Abdelhameed <edgepro.mhameed@gmail.com>
2024-12-08 00:23:51 +0200

    adding custom hook

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -3,49 +3,26 @@ import { useNavigate, useParams, Link, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import NotFound from "../components/NotFound";
 import DefinitionSearch from "../components/DefinitionSearch";
+import useFetch from "../hooks/UseFetch";
 
 export default function Definition() {
-  const [word, setWord] = useState();
-  const [notFound, setNotFound] = useState(false);
-  const [error, setError] = useState(false);
+  //const [word, setWord] = useState();
+  //const [notFound, setNotFound] = useState(false);
+  //const [error, setError] = useState(false);
   let { search } = useParams();
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const location = useLocation();
+  const [word, errorStatus] = useFetch(
+    "https://api.dictionaryapi.dev/api/v2/entries/en/" + search
+  );
 
   useEffect(() => {
-    //const url ="https://xcklhklvhdfklhdfjklghdfjkghjkdhgk.com";
-    //const url ="https://httpstat.us/500";
-    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
-    fetch(url)
-      .then((response) => {
-        if (response.status === 404) {
-          setNotFound(true);
-        } else if (response.status === 401) {
-          navigate("/login", {
-            state: {
-              previousUrl: location.pathname,
-            },
-          });
-        } else if (response.status === 500) {
-           setError(true);
-        }
+    console.log("word: ", word, "errorStatus: ", errorStatus);
+  })
 
-        if(!response.ok) {
-          setError(true);
-          throw new Error("Something went wrong");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setWord(data[0].meanings); // Set the meanings to `word`
-      })
-      .catch((e) => {
-        console.log(e.message)
-      }); // Log any fetch errors
-  }, []);
-
-  if (notFound === true) {
+ 
+  if (errorStatus === 404) {
     return (
       <>
         <NotFound />
@@ -53,7 +30,7 @@ export default function Definition() {
       </>
     );
   }
-  if (error === true) {
+  if (errorStatus) {
     return (
       <>
         <p>Something went wrong, try again?</p>
@@ -61,14 +38,13 @@ export default function Definition() {
       </>
     );
   }
-
-
+ 
   return (
     <>
-      {word ? (
+      {word?.[0]?.meanings? (
         <>
           <h1>Here is a definition:</h1>
-          {word.map((meaning) => {
+          {word[0].meanings.map((meaning) => {
             return (
               <p key={uuidv4()}>
                 {meaning.partOfSpeech + ": "}
